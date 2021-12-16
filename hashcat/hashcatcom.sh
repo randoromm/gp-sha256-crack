95625347d5566c77d1bde0e4f92395b4c4f0c0b4
Rando Rommot <r.rommot@gmail.com>
2021-12-16 17:04:54 +0200

    Updated hashcat charshets and script to run with GPU as well on OpenCL intel HD

--- hashcat/hashcatcom.sh
+++ hashcat/hashcatcom.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-hashcat -a 3 -m 1400 -1 diglet.hcchr -2 ot.hcchr -3 zon.hcchr -4 em.hcchr sha.hash '?1?1?1?1 N59 2?3.?d?d?d E024 4?4.?d?d?d' -O
+hashcat -a 3 -m 1400 -1 diglet.hcchr -2 ot.hcchr -3 zon.hcchr -4 em.hcchr sha.hash '?1?1?1?1 N59 20.?d?d?d E024 4?4.?d?d?d' -O --force
